da0a07c720bc5da7e4cfac9c1cbac0b3b59062b7
zhang bowen <2585783520@qq.com>
2024-03-09 08:33:07 +0000

    environment and test for exe

--- reset.py
+++ reset.py
@@ -1,10 +1,12 @@
 import util.MIG_operator as MIG_operator
 import util.MPS_operator as MPS_operator
+num_gpu = 2
 
-UUID_list = MIG_operator.get_uuid(0)
+for i in range(0, num_gpu):
+    UUID_list = MIG_operator.get_uuid(i)
 
 
-for i in UUID_list:
-    MPS_operator.CloseMPS(i)
+    for j in UUID_list:
+        MPS_operator.CloseMPS(j)
 
-MIG_operator.reset_mig(0)
+    MIG_operator.reset_mig(i)
--- script/test.sh
+++ script/test.sh
@@ -4,9 +4,9 @@
 offline_model_list=("GAN" "transformer" "bert" "resnet50" "resnet152" "mobilenet" "deeplabv3" "SqueezeNet" "unet" "vit")
 online_model_list=("resnet50"  "resnet152" "vgg19" "vgg16" "inception_v3" "unet" "deeplabv3" "mobilenet_v2" "alexnet" "bert")
 
-# for model1 in ${offline_model_list[@]}; do
-#     cd /data/zbw/MIG/MIG/ATC-MIG/jobs/offline && proxychains python entry.py --task $model1  --epoch 1 
-# done
+for model1 in ${offline_model_list[@]}; do
+    cd /data/zbw/MIG/MIG/ATC-MIG/jobs/offline && proxychains python entry.py --task $model1  --epoch 1 
+done
 
 
 for model1 in ${online_model_list[@]}; do
